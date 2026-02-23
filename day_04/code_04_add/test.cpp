4a29be7b28a1211ea9e3a9c4dcd61a861b467f9f
zyh <2941412991@qq.com>
2026-02-23 14:21:07 +0800

    day_94_cpde_add

--- day_04/code_04_add/test.cpp
+++ day_04/code_04_add/test.cpp
@@ -153,20 +153,3 @@ using namespace std;
 //	return 0;
 //}
 
-
-#include"Date.h"
-int main()
-{
-	Date d1(2024, 7, 12);
-	Date d2 = d1 + 100;
-	//Date d3(d1 + 100);
-	d1.Print();
-	d2.Print();
-
-	//d1 += 100;
-	//d1.Print();
-
-	d1 += 30000;
-	d1.Print();
-	return 0;
-}
