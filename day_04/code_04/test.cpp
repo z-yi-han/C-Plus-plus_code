c57190ebad027be1b22e53d3380796d8f3464a39
zyh <han20051122@gmail.com>
2026-03-18 00:11:44 +0800

    Add files via upload

--- day_04/code_04/test.cpp
+++ day_04/code_04/test.cpp
@@ -5,30 +5,30 @@ using namespace std;
 //class Date
 //{
 //public:
-//	// 1.无参构造函数
-//	/*Date()
-//	{
-//		_year = 1;
-//		_month = 1;
-//		_day = 1;
-//	}*/
-//  
+//	//// 1.无参构造函数
+//	//Date()
+//	//{
+//	//	_year = 1;
+//	//	_month = 1;
+//	//	_day = 1;
+//	//}
+// // 
 //
-//	// 2.带参构造函数
-//	/*Date(int year, int month, int day)
-//	{
-//		_year = year;
-//		_month = month;
-//		_day = day;
-//	}*/
+//	//// 2.带参构造函数
+//	//Date(int year, int month, int day)
+//	//{
+//	//	_year = year;
+//	//	_month = month;
+//	//	_day = day;
+//	//}
 //
 //	// 3.全缺省构造函数
-//	/*Date(int year = 1, int month = 1, int day = 1)
+//	Date(int year = 1, int month = 1, int day = 1)
 //	{
 //		_year = year;
 //		_month = month;
 //		_day = day;
-//	}*/
+//	}
 //
 //	void Print()
 //	{
@@ -45,11 +45,11 @@ using namespace std;
 //	Date d1;
 //	d1.Print();
 //
-//	/*Date d2(2024, 7, 10);
-//	d2.Print();*/
+//	Date d2(2024, 7, 10);
+//	d2.Print();
 //
-//	/*Date d3(2024);
-//	d3.Print();*/
+//	Date d3(2024);
+//	d3.Print();
 //
 //	//Date func();
 //	//func.Print();
@@ -58,9 +58,7 @@ using namespace std;
 //}
 
 
-//#include<iostream>
-//using namespace std;
-//
+
 //typedef int STDataType;
 //class Stack
 //{
@@ -116,8 +114,8 @@ using namespace std;
 ////	return 0;
 
 
-//#include<iostream>
-//using namespace std;
+
+
 //// 用最新加了构造和析构的C++版本Stack实现
 //bool isValid(const char* s) {
 //	Stack st;
@@ -198,4 +196,174 @@ using namespace std;
 //	return 0;
 //}
 
+
+
+//#include<iostream>
+//using namespace std;
+//class Date {
+//public:
+//	Date(int year = 1,int month=1,int day=1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	Date(const Date& d)//const保护实参不被改变
+//	{
+//		//if (d._day = _day)
+//		//{
+//		//	//........
+//		//}
+//		_year = d._year;
+//		_month = d._month;
+//			_day = d._day;
+//	}
+//	void Print()
+//	{
+//		cout << _year<<" " << _month<<" " << _day << endl;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//void Func1(Date d){
+//	cout << &d << endl;
+//	d.Print();
+//}
+//
+//
+//int main()
+//{
+//	Date d1(2026, 2, 21);
+//	d1.Print();
+//	Func1(d1);
+//	/*Date d2(d1);
+//	d2.Print();*/
+//	return 0;
+//}
+
+
+
+//typedef int STDataType;
+//class Stack
+//{
+//public:
+//	Stack(int n = 4)
+//	{
+//		_a = (STDataType*)malloc(sizeof(STDataType) * n);
+//		if (nullptr == _a)
+//		{
+//			perror("malloc申请空间失败");
+//			return;
+//		}
+//		_capacity = n;
+//		_top = 0;
+//	}
+//
+//
+//	// st2(st1)
+//	Stack(const Stack& st)
+//	{
+//		cout << "Stack(const Stack& st)" << endl;
+//
+//		// 需要对_a指向资源创建同样大的资源再拷贝值
+//		_a = (STDataType*)malloc(sizeof(STDataType) * st._capacity);
+//		if (nullptr == _a)
+//		{
+//			perror("malloc申请空间失败!!!");
+//			return;
+//		}
+//		memcpy(_a, st._a, sizeof(STDataType) * st._top);
+//		_top = st._top;
+//		_capacity = st._capacity;
+//	}
+//
+//	void Push(STDataType x)
+//	{
+//		if (_top == _capacity)
+//		{
+//			int newcapacity = _capacity * 2;
+//			STDataType* tmp = (STDataType*)realloc(_a, newcapacity *
+//				sizeof(STDataType));
+//			if (tmp == NULL)
+//			{
+//				perror("realloc fail");
+//				return;
+//			}
+//			_a = tmp;
+//			_capacity = newcapacity;
+//		}
+//		_a[_top++] = x;
+//
+//	}
+//	~Stack()
+//	{
+//		cout << "~Stack()" << endl;
+//		free(_a);
+//		_a = nullptr;
+//		_top = _capacity = 0;
+//	}
+//private:
+//	STDataType* _a;
+//	size_t _capacity;
+//	size_t _top;
+//};
+//void func(Stack st)
+//{
+//
+//}
+////int main()
+////{
+////	Stack st1;
+////	st1.Push(1);
+////	st1.Push(2);
+////
+////	// Stack不显示实现拷贝构造，用自动生成的拷贝构造完成浅拷贝
+////	// 会导致st1和st2里面的_a指针指向同一块资源，析构时会析构两次，程序崩溃
+////	Stack st2(st1);
+////
+////	return 0;
+////}
+
+
+//// 两个Stack实现队列
+//class MyQueue
+//{
+//public:
+//private:
+//	Stack pushst;
+//	Stack popst;
+//};
+//
+//int main()
+//{
+//	Stack st1;
+//	st1.Push(1);
+//	st1.Push(2);
+//
+//	func1(st1);
+//
+//	// Stack不显示实现拷贝构造，用自动生成的拷贝构造完成浅拷贝
+//	// 会导致st1和st2里面的_a指针指向同一块资源，析构时会析构两次，程序崩溃
+//	Stack st2(st1);
+//	Stack st3 = st1;
+//
+//	MyQueue mq1;
+//	// MyQueue自动生成的拷贝构造，会自动调用Stack拷贝构造完成pushst/popst
+//	// 的拷贝，只要Stack拷贝构造自己实现了深拷贝，他就没问题
+//	MyQueue mq2(mq1);
+//
+//	return 0;
+//}
+
+//Stack& func2()
+//{
+//	static Stack st;//不加static的话出了作用域st就不在了
+//	return st;
+//}
+//int main()
+//{
+//	Stack ret = func2();
+//}
 
